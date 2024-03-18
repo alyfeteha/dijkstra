86789090405a220b0106f60cfcdcd547aefcdedd
alyfeteha <48603074+alyfeteha@users.noreply.github.com>
2024-03-18 15:54:53 +0200

    Update ShortestPath.java

--- dijkstra/src/ShortestPath.java
+++ dijkstra/src/ShortestPath.java
@@ -2,9 +2,6 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class ShortestPath {
-
-
-
     public void insertInGraph(int src, int dest, int time, int cost, Node[] adList,int m)
     {
         Node desti=new Node(dest,cost,time);
@@ -26,14 +23,12 @@ public class ShortestPath {
         if(head.next==null){
             head.next=node;
             node.next=null;
-            System.out.println("insert after -->"+head.value);
         }
         else{
             insertInList(head.next,node);
         }
 
     }
-
 
     public void sortPriority(Node [] list,int source,int destination,int cost){
         PriorityQueue<Node> neighbours = new PriorityQueue<>(list.length,new CityComparator());
@@ -44,56 +39,35 @@ public class ShortestPath {
             neighbours.add(list[i]);
         }
         buildSP(list,neighbours,source,destination,cost,0);
-
     }
 
     public void buildSP(Node [] list,PriorityQueue<Node> neighbours,int source, int destination,int cost,int t)
     {
-
             for(int i=0;i<list.length;i++)
             {
-
                     Node src = list[neighbours.poll().value - 1];
                 if(src.next!=null)
                 {
                     Node dest = list[src.next.value - 1];
-
-
                     isSmaller(list, src, dest, src.next, cost);
-
-
                     neighbours.clear();
                     neighbours.addAll(Arrays.asList(list));
                     for (int j = -1; j < i; j++) {
                         neighbours.poll();
                     }
                 }
-
             }
     }
 
-
-
-
-
-
-
     public void isSmaller(Node []list,Node src,Node dest,Node next,int cost) {
-
-//        for(int i=0;i<list.length;i++)
-//            System.out.println(" ana "+list[i].value+" time bta3y: "+list[i].time+" t bta3y "+list[i].t+"   d bta3y "+list[i].d);
 
         int calc = 0;
         if (src.parent != null) {
-
             calc = src.d + next.d + (src.t + 1) * cost;
-//            System.out.println("ana "+ next.value+ " 3andy ahl bey2oolo: "+calc+" m3aya:"+next.d);
-
         }
         else
             {
             calc = src.d + next.d + src.t * cost;
-//            System.out.println("ana "+ src.value+ " bey2oolo: "+calc+" ma3aya "+next.d);
             }
         if (calc<dest.d)
         {
@@ -106,13 +80,10 @@ public class ShortestPath {
             dest.t=dest.parent.t+1;
             else
                 dest.t=0;
-
         }
         if(next.nextnotNull())
         {
             isSmaller(list,src, list[next.next.value - 1],next.next, cost);
         }
-
-
     }
 }
